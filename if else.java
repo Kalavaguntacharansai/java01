1eb31a3630d6f5978f6b73ec35e775f6d3e8dccb
k sai charan <141731843+Kalavaguntacharansai@users.noreply.github.com>
2023-12-29 12:18:36 +0530

    Add files via upload

---
+++ if else.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class  Biggest
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int a,b;
+		System .out.println("enter two nos");
+		a=s.nextInt();
+		b=S.nextInt();
+		if(a>b);
+		{
+		    System.out.println(a+"is biggest");
+		}
+		else
+		{
+			System.out.println(b+"is biggest");
+	}
+}
+}
