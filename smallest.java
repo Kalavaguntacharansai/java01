c9bb1bc565d9f27188cc80e7d25dd641ee0d383e
k sai charan <141731843+Kalavaguntacharansai@users.noreply.github.com>
2023-12-29 12:43:07 +0530

    Add files via upload

---
+++ smallest.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class  Biggest
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int a,b;
+		System .out.println("enter three nos");
+		a=s.nextInt();
+		b=S.nextInt();
+		c=s.nextInt();
+		if(a<b && a<c);
+		{
+		    System.out.println(a+"is smallest");
+		}
+		else
+		if(b<c)
+		{
+			System.out.println(b+"is smallest");
+	}
+}
+}
