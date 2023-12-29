637074115ef52967275c77c23aaa5e9941c56d45
k sai charan <141731843+Kalavaguntacharansai@users.noreply.github.com>
2023-12-29 15:15:54 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class  Test
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int a;
+		System .out.println("enter  no");
+		a=s.nextInt();
+		b=S.nextInt();
+		if(a>0);
+		{
+		    System.out.println(+a"is positive");
+		}
+		else
+		{
+			System.out.println(+a"is neagative");
+	}
+	else
+		{
+		System.out.println(+a"is zero");
+		}
+	}
+}
+
