e985bbc9abdd2e8755d71def035ce6dbdb1e2dd8
k sai charan <141731843+Kalavaguntacharansai@users.noreply.github.com>
2023-12-29 14:36:00 +0530

    Add files via upload

---
+++ nested.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class  evenodd
+{
+	public static void main(String[] args) 
+	{
+		scanner s=new scanner(system.in);
+		int a;
+		System.out.println("enter a no");
+		a=s.nextInt();
+		if(a%2==0)
+		{
+				System.out.println("even no");
+			}
+			else
+			{
+				System.out.println("odd no");
+			}
+		}
+	}
+
+
