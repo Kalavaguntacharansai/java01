1be98a1a483a57a6c81062679073370e8bde1460
k sai charan <141731843+Kalavaguntacharansai@users.noreply.github.com>
2023-12-29 11:22:18 +0530

    Add files via upload

---
+++ division.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class  power
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		System .out.println("enter two nos");
+		a=s.nextInt();
+		b=S.nextInt();
+		if(b==0);
+		{
+		    System.out.println("second no cannot be zero");
+		    b=1;
+		}
+		r= a\b;
+			System.out.println("division="+r);
+	}
+}
