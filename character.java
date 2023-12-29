8eedb4dd68166fa623757345344c3f48c951617b
k sai charan <141731843+Kalavaguntacharansai@users.noreply.github.com>
2023-12-29 14:56:40 +0530

    Add files via upload

---
+++ character.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class  Test
+{
+	public static void main(String[] args) 
+	{
+		scanner s=new scanner(system.in);
+		char a;
+		System.out.println("enter a string");
+		a=s.next().charAt(0);
+		if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'|| a=='A'||a=='E'||a=='I'||a=='O'||a=='U'||)
+		{
+				System.out.println("vowel");
+			}
+			else
+			{
+				System.out.println("consonant");
+			}
+		}
+	}
+
+
