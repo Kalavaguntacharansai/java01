d08d3afac524d3f32ca04bbfe2ce62df42553f37
k sai charan <141731843+Kalavaguntacharansai@users.noreply.github.com>
2024-01-02 09:46:36 +0530

    Add files via upload

---
+++ multiple.java
@@ -0,0 +1,15 @@
+class loop1
+{
+	public static void main(String[] args)
+	{
+	    scanner s=new scanner (System.in);
+	    int i,n;
+	    n=s.nextInt();
+	    for (i=1;i<=10;i++)
+		{
+		System.out.println(i+"*"+n+"="+i*n);
+		    
+		}
+}
+}
+
