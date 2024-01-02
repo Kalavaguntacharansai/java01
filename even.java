61d4c3c553ca99d0e93e6963eaedccefb8df434e
k sai charan <141731843+Kalavaguntacharansai@users.noreply.github.com>
2024-01-02 09:46:15 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,12 @@
+import.jav.util.*;
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    scanner s=new scanner (System.in);
+	    int=1;
+	    n=s.nextInt();
+	    for (i=2;i<=n;i=i+1)
+		System.out.println(i+"");
+	}
+}
