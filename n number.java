1f362b83334cf2e8630bb45ee086a494dc10a689
k sai charan <141731843+Kalavaguntacharansai@users.noreply.github.com>
2024-01-02 09:46:56 +0530

    Add files via upload

---
+++ n number.java
@@ -0,0 +1,15 @@
+import.jav.util.*;
+class loop
+{
+	public static void main(String[] args)
+	{
+	    scanner s=new scanner (System.in);
+	    int=i,n,r=0;
+	    n=s.nextInt();
+	    for (i=1;i<=n;i++)
+		{
+			r=r+i;
+		}
+		System.out.println(r);
+	}
+}
