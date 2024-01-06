7dff354e8a21ece111b896b447be5ce23530d169
k sai charan <141731843+Kalavaguntacharansai@users.noreply.github.com>
2024-01-06 12:35:24 +0530

    Add files via upload

---
+++ Arth.java
@@ -0,0 +1,20 @@
+package sai;
+public class Arth
+{
+	public int add(int a,int b);
+	{
+		return a+b;
+	}
+	public int sub (int a,int b);
+	{
+		return a-b;
+	}
+	public int mul(int a,int b);
+	{
+		return a*b;
+	}
+	piblic int div(int a,int b);
+	{
+		return a/b;
+	}
+}
